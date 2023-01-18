b6385f884a7f5c7471eeed348663a0e358aa0473
Chrono <me@disilla.org>
2023-01-18 12:42:57 -0500

    update

--- src/Katana.js
+++ src/Katana.js
@@ -7,9 +7,19 @@
 const fs = require("fs");
 const path = require("path");
 
+function initSaving() {
+	this.library = JSON.parse(fs.readFileSync(this.libPath, "utf8"));
+	this.store = JSON.parse(fs.readFileSync(this.strPath, "utf8"));
+	if (this.encryptOpt.enable && fs.existsSync(this.encryptOpt.seedPath)) {
+		this.encryptOpt.seed = fs.readFileSync(this.encryptOpt.seedPath, "utf8");
+		this.decrypt();
+	}
+	if (this.store.length != Object.keys(this.library).length) throw new Error("Store & Library Size Mismatch, Please manually repair!");
+}
+
 /**
 	* Katana Datastore
-	* @param {String} dbPath Relative path to the datastorefolder
+	* @param {String} dbPath Relative path to the datastore folder
 	* @param {Object} options Object containing options for the datastore
 	* @ignore
 */
@@ -18,39 +28,43 @@ class Katana {
 		this.strPath = path.join(dsPath, "store.json");
 		this.libPath = path.join(dsPath, "library.json");
 		this.encryptOpt = {
-			enable: options.encrypt,
-			seedPath: path.join(dsPath, "seed.txt"),
-			seed: 0
+			enable: options.encrypt
 		};
+		if (this.encryptOpt.enable) {
+			this.encryptOpt.seedPath = path.join(dsPath, "seed.txt")
+			this.encryptOpt.seed = 0;
+		}
+
 		this.store = [];
 		this.library = {};
 
 		if (options.saveToDisk) {
+			if(!fs.existsSync(dsPath)) fs.mkdirSync(dsPath);
 			if (fs.existsSync(this.strPath) && fs.existsSync(this.libPath)) {
-				this.library = JSON.parse(fs.readFileSync(this.libPath, "utf8"));
-				this.store = JSON.parse(fs.readFileSync(this.strPath, "utf8"));
-				if (this.encryptOpt.enable && fs.existsSync(this.encryptOpt.seedPath)) {
-					this.encryptOpt.seed = fs.readFileSync(this.encryptOpt.seedPath, "utf8");
-					this.decrypt();
-				}
-				if (this.store.length != Object.keys(this.library).length) throw new Error("Store & Library Size Mismatch, Please manually repair!");
+				initSaving();
+			} else {
+				fs.mkdirSync(this.strPath);
+				fs.mkdirSync(this.libPath);
+				initSaving();
 			}
 
 			process.addListener("beforeExit", () => {
-				if (this.encryptOpt.enable) this.encrypt();
-				fs.writeFileSync(this.encryptOpt.seedPath, `${this.encryptOpt.seed}`);
+				if (this.encryptOpt.enable) {
+					this.encrypt();
+					fs.writeFileSync(this.encryptOpt.seedPath, `${this.encryptOpt.seed}`);
+				}
 				this.saveState();
 			});
 		}
 	}
 
 	/**
- 		* Writes data to the datastore
- 		* @param {String} data Text to write to the datastore
- 		* @param {String} key Key you want to store the data under
- 		* @ignore
-		* @returns {void}
- 	*/
+			* Writes data to the datastore
+			* @param {String} data Text to write to the datastore
+			* @param {String} key Key you want to store the data under
+			* @ignore
+		* @returns {Boolean} Returns true if the key if the write was successful or false if not.
+	  */
 	write(data, key) {
 		if (this.library[key]) throw new Error("Key already exists");
 		try {
@@ -58,58 +72,57 @@ class Katana {
 			this.library[key] = this.store.length - 1;
 			this.store[this.library[key]] = this.encode(this.store[this.library[key]]);
 		} catch {
-			throw new Error("Failed to write to datastore");
+			return false;
 		}
+		return true;
 	}
 
 	/**
- 		* Writes data to the datastore & overwrites existing data under the same key.
- 		* @param {String} data Text to write to the datastore
- 		* @param {String} key Key you want to store the data under (MUST ALREADY EXIST)
- 		* @ignore
-		* @returns {void}
- 	*/
+			* Writes data to the datastore & overwrites existing data under the same key.
+			* @param {String} data Text to write to the datastore
+			* @param {String} key Key you want to store the data under (MUST ALREADY EXIST)
+			* @ignore
+		* @returns {void} Returns true if the key if the write was successful or false if not.
+	  */
 	overwrite(data, key) {
 		try {
 			this.delete(key);
 		} catch {
-			throw new Error("Key does not already exist");
+			return false;
 		}
-		this.write(data, key);
+		return this.write(data, key);
 	}
 
 	/**
- 		* Retrieves data from the datastore
- 		* @param {String} key Key you want to store the data under (MUST ALREADY EXIST)
- 		* @ignore
+			* Retrieves data from the datastore
+			* @param {String} key Key you want to store the data under (MUST ALREADY EXIST)
+			* @ignore
 		* @returns {String} Data stored under the key
- 	*/
+	  */
 	get(key) {
 		if (this.store[this.library[key]] == undefined) throw new Error("Key does not exist");
 		return this.decode(this.store[this.library[key]]);
 	}
 
 	/**
- 		* Deletes an entry in the datastore
- 		* @param {String} key Key you want to delete
- 		* @ignore
-		* @returns {Boolean}
- 	*/
+			* Deletes an entry in the datastore
+			* @param {String} key Key you want to delete
+			* @ignore
+		* @returns {Boolean} Returns true if the delete was successful or false if not.
+	  */
 	delete(key) {
-		if(this.library[key] == undefined) throw new Error("Trying to delete non-existent key");
+		if (this.library[key] == undefined) throw new Error("Trying to delete non-existent key");
 		this.store.splice(this.store.indexOf(this.library[key]), 1);
 		delete this.library[key];
-		try {
-			if (this.get(key)) throw new Error("Failed to delete key");
-		} catch {
-			return true;
-		}
+		if (this.has(key)) {
+			return false;
+		} else return true;
 	}
 
 	/**
- 		* Check if datastore is has a entry for the given key
- 		* @param {String} key String to check for
- 		* @ignore
+			* Check if datastore is has a entry for the given key
+			* @param {String} key String to check for
+			* @ignore
 		* @returns {Boolean} Returns true if the key exists or false if not.
 	*/
 	has(key) {
@@ -117,29 +130,29 @@ class Katana {
 	}
 
 	/**
- 		* Encodes a string into an array of numbers
- 		* @param {String} entry String to encode
- 		* @ignore
+			* Encodes a string into an array of numbers
+			* @param {String} entry String to encode
+			* @ignore
 		* @returns {Array} Array of numbers
- 	*/
+	  */
 	encode(entry) {
 		return entry.split("").map((char) => char.charCodeAt(0));
 	}
 
 	/**
- 		* de-encodes a array of numbers into a string
- 		* @param {Array} entry Key you want to delete
- 		* @ignore
+			* de-encodes a array of numbers into a string
+			* @param {Array} entry Key you want to delete
+			* @ignore
 		* @returns {String} Array de-encoded
- 	*/
+	  */
 	decode(array) {
 		return array.map((num) => String.fromCharCode(num)).join("");
 	}
 
 	/**
- 		* "Encrypts" entire live datastore
- 		* @ignore
- 	*/
+			* "Encrypts" entire live datastore
+			* @ignore
+	  */
 	encrypt() {
 		this.encryptOpt.seed = Math.fround(Math.random() * 100000);
 		this.store = this.store.map(entry => {
@@ -148,9 +161,9 @@ class Katana {
 	}
 
 	/**
- 		* "De-encrypts" entire live datastore
- 		* @ignore
- 	*/
+			* "De-encrypts" entire live datastore
+			* @ignore
+	  */
 	decrypt() {
 		this.store = this.store.map((entry) => {
 			return entry.map((num) => num / this.encryptOpt.seed);
@@ -158,10 +171,10 @@ class Katana {
 	}
 
 	/**
- 		* "Encrypts" entire live datastore
- 		* @ignore
+			* "Encrypts" entire live datastore
+			* @ignore
 		* @returns {Array} Containing the library & store
- 	*/
+	  */
 	exportState() {
 		this.decrypt();
 		let x = this.library;
@@ -171,18 +184,18 @@ class Katana {
 	}
 
 	/**
- 		* Hot-wipes the datastore
- 		* @ignore
- 	*/
+			* Hot-wipes the datastore
+			* @ignore
+	  */
 	purgeState() {
 		this.store = [];
 		this.library = {};
 	}
 
 	/**
- 		* Saves the datastore to disk
- 		* @ignore
- 	*/
+			* Saves the datastore to disk
+			* @ignore
+	  */
 	saveState() {
 		fs.writeFileSync(this.strPath, JSON.stringify(this.store));
 		fs.writeFileSync(this.libPath, JSON.stringify(this.library));
